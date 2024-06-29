2412b104ea426435af1d0cbb953069538910263c
rakibaakter <rakibaakter635@gmail.com>
2024-06-29 14:03:14 +0600

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>todo app</h1>
+      <Button>My button</Button>
+    </>
+  );
+}
+
+export default App;
